48a08c73aae0e281065d95bb69275961728bf959
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-08-18 14:53:21 +0200

    movement
    Created basic movement

---
+++ Gymnasiearbete/Assets/Scripts/cameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = new Vector3(0, 2, -5);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform);
+        transform.position = player.transform.position + _offset;
+    }
+}
---
+++ Gymnasiearbete/Assets/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float xInput;
+    public float zInput;
+    public float movementSpeed;
+    public Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");       //Gets Input for use on X-axis
+        zInput = Input.GetAxis("Vertical");         //Gets input for use on Z-axis
+        movement = Vector3.forward * zInput * movementSpeed * Time.deltaTime + Vector3.right * xInput * movementSpeed * Time.deltaTime;     //uses prebuilt vectors and adapts them to create a movement vector
+
+        transform.Translate(movement);      //Applies new movement vector to gameObject
+    }
+}
