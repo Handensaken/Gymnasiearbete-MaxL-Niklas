dfa007f60c6a91c5e3b7eb9e20213bfb19b36d61
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-08-21 16:48:17 +0200

    move camera with mouse
    made camera moveable with mouse, limitations are required.

--- Gymnasiearbete/Assets/Scripts/cameraController.cs
+++ Gymnasiearbete/Assets/Scripts/cameraController.cs
@@ -13,28 +13,35 @@ public class cameraController : MonoBehaviour
 
     public float yaw;
     public float pitch;
+
+    public float playerDistance;
     // Start is called before the first frame update
     void Start()
     {
         _offset = new Vector3(0, 2, -5);
         hSpeed = 2.0f;
         vSpeed = 2.0f;
+        playerDistance = -5.0f;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mhm = Input.GetAxis("Mouse X");
-        //transform.LookAt(player.transform);
-        if (player.transform.position.y >= -4)
-        {
-            transform.position = Vector3.Slerp(transform.position, player.transform.position + _offset, 0.1f);
-        }
-        yaw += hSpeed * mhm;
-        pitch -= vSpeed * Input.GetAxis("Mouse Y");
+        float cameraAngleY = (Mathf.PI / 180) * transform.eulerAngles.y;
+        float cameraAngleX = (Mathf.PI / 180) * transform.eulerAngles.x;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+
+        yaw += hSpeed * mouseX;
+        pitch -= vSpeed * mouseY;
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-        print(yaw);
+        if (player.transform.position.y >= -4)
+        {
+            _offset = new Vector3(Mathf.Sin(cameraAngleY) * playerDistance, Mathf.Sin(cameraAngleX) * 5, Mathf.Cos(cameraAngleY + cameraAngleX) * playerDistance);
+            transform.position = Vector3.Slerp(transform.position, player.transform.position + _offset, 0.1f);
+        }
     }
 }
